0de20c4b73d9fd8539dd49c354480535365449f2
astro-nicks <astronix05@gmail.com>
2024-05-19 22:34:34 +0000

    Array Manipulation

---
+++ arrayManipulation.js
@@ -0,0 +1,21 @@
+
+function processArray(numbers) {
+    return numbers.map(num => num % 2 === 0 ? num * num : num * 3);
+}
+
+// Task 2: Function to format an array of strings based on corresponding numbers
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((str, index) => {
+        return processedNumbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
+
+// Example usage
+const numbers = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(numbers); // [3, 4, 9, 16, 15]
+
+const strings = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+const formattedStrings = formatArrayStrings(strings, processedNumbers); // ['apple', 'BANANA', 'cherry', 'DATE', 'elderberry']
+
+console.log(processedNumbers);
+console.log(formattedStrings);
